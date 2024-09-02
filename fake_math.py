c5acc7c82a93e9980dde1a38e92ff3119e6a6ad4
Kit354 <kit354@mail.ru>
2024-09-03 00:42:15 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    elif first / second:
+        result = first / second
+        return result
+
+
+divide(69, 3)
+divide(3, 0)
